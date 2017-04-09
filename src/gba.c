5939f8f2627fb976fbe0342174d760b53a02eb08
hxtk <peter@psanders.me>
2017-04-09 18:25:14 -0400

    Switch byte order in `gba_set_pixel()` to fix render errors

--- src/gba.c
+++ src/gba.c
@@ -23,17 +23,18 @@ void gba_refresh() {
 }
 
 void gba_fill_buffer(int color_id) {
-  // It is only necessary to compute the color block once because it is reused
-  // for all pixels and we do not care about battering the neighboring values.
-  uint16_t color_block = color_id | ((uint16_t)(color_id) << 8);
-
   // If this bit is set then the first buffer is the working buffer (because
   // the second buffer is the display buffer). To prevent screen tearing, we
   // only allow the working buffer to be modified.
   volatile uint16_t (*const buffer)[120] = (*gba_display_control & 0x10) ?
                                            gba_color_mat_front :
                                            gba_color_mat_back;
-  
+
+  // It is only necessary to compute the color block once because it is reused
+  // for all pixels and we do not care about battering the neighboring values.
+  uint16_t color_block = color_id | ((uint16_t)(color_id) << 8);
+
+  // Set the entire buffer to the color block defined above
   for (int i = 0; i < GBA_MAX_Y; ++i) {
     for (int j = 0; j*2 < GBA_MAX_X; ++j) {
       buffer[i][j] = color_block;
@@ -45,22 +46,24 @@ int gba_set_pixel(int x, int y, int color_id) {
   if (color_id < 0 || color_id >= 256) return false;
   if (y >= GBA_MAX_Y || y < 0 || x >= GBA_MAX_X || x < 0) return false;
 
-  // Only allow the least significant byte of |color_id| to be set
-  color_id &= 0xff;
-
   // If this bit is set then the first buffer is the working buffer (because
   // the second buffer is the display buffer). To prevent screen tearing, we
   // only allow the working buffer to be modified.
   volatile uint16_t (*const buffer)[120] = (*gba_display_control & 0x10) ?
                                            gba_color_mat_front :
                                            gba_color_mat_back;
-  
+
+  // Each 16-bit block is two bytes responsible for different pixels which cannot
+  // be altered independently. First we mask out the byte we will be altering
+  // by ANDing the pair with 0x00 for the byte to clear and 0xff for the byte
+  // that should be unchanged. Since `0x00 | n == n`, we then OR the block with
+  // the desired byte, shifted into the proper position as necessary.
   if (x & 1) {
-    buffer[y][x/2] &= 0xff00;
-    buffer[y][x/2] |= (uint16_t)color_id;
-  } else {
     buffer[y][x/2] &= 0x00ff;
     buffer[y][x/2] |= (uint16_t) (color_id) << 8;
+  } else {
+    buffer[y][x/2] &= 0xff00;
+    buffer[y][x/2] |= (uint16_t) (color_id);
   }
 
   return true;
